1687d31fa6917e5d441b2b15427354a052387d95
franaragm <franaragonmesa@gmail.com>
2019-03-07 16:03:23 +0100

    section 6 (83) alert dialogs

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -7,6 +7,24 @@ class ProductPage extends StatelessWidget {
 
   ProductPage(this.title, this.imageUrl);
 
+  _showWarningDialog(BuildContext context) {
+    showDialog(context: context, builder: (BuildContext context){
+      return AlertDialog(
+        title: Text('Are you sure?'),
+        content: Text('This action cannot be undone!'),
+        actions: <Widget>[
+          FlatButton(child: Text('DISCARD'), onPressed: () {
+            Navigator.pop(context);
+          },),
+          FlatButton(child: Text('CONTINUE'), onPressed: () {
+            Navigator.pop(context);
+            Navigator.pop(context, true);
+          },),
+        ],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -34,11 +52,13 @@ class ProductPage extends StatelessWidget {
                     child: RaisedButton(
                       color: Theme.of(context).accentColor,
                       child: Text('Delete'),
-                      onPressed: () => Navigator.pop(context, true),
+                      onPressed: () => _showWarningDialog(context),
                     ),
                   ),
                 ],
               ),
-            )));
+            )
+        )
+    );
   }
 }
