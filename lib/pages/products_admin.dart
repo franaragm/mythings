8ee5a77d6be1acf4f3e7612afc867083008ae066
franaragm <franaragonmesa@gmail.com>
2019-03-07 12:44:14 +0100

    section 6 (76, 77) adding tab navigation

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mythings/pages/products_page.dart';
 
+import 'package:mythings/pages/product_create.dart';
+import 'package:mythings/pages/product_list.dart';
+
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        drawer: Drawer(
-            child: Column(
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            drawer: Drawer(
+                child: Column(
               children: <Widget>[
                 AppBar(
                   automaticallyImplyLeading: false,
@@ -19,17 +24,31 @@ class ProductsAdminPage extends StatelessWidget {
                     Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(
-                            builder: (BuildContext context) => ProductsPage())
-                    );
+                            builder: (BuildContext context) => ProductsPage()));
                   },
                 )
               ],
             )),
-        appBar: AppBar(
-          title: Text('Manage Products'),
+            appBar: AppBar(
+              title: Text('Manage Products'),
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    icon: Icon(Icons.create),
+                    text: 'Create Product',
+                  ),
+                  Tab(
+                    icon: Icon(Icons.list),
+                    text: 'My Products',
+                  )
+                ],
+              ),
+            ),
+            body: TabBarView(children: <Widget>[
+              ProductCreatePage(),
+              ProductListPage()
+            ],),
         ),
-        body: Center(
-          child: Text('Manage your Products'),
-        ));
+    );
   }
 }
