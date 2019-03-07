810d1373cd35d6561612ae9ec6ad1986cf23be35
franaragm <franaragonmesa@gmail.com>
2019-03-07 12:04:19 +0100

    section 6 (74) navegación entre enlaces del sidebar

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:mythings/product_manager.dart';
+import 'package:mythings/pages/products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+        drawer: Drawer(
+            child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductsAdminPage())
+                );
+              },
+            )
+          ],
+        )),
         appBar: AppBar(
           title: Text('ListThings'),
         ),
-        body: ProductManager()
-    );
+        body: ProductManager());
   }
 }
