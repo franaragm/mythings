0076f7bec3819351dc746de2e9ff042d6f5797af
franaragm <franaragonmesa@gmail.com>
2019-03-12 11:00:08 +0100

    section 9 close keyboard

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -91,34 +91,39 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              child: Text('Save'),
-              color: Theme.of(context).accentColor,
-              textColor: Colors.white,
-              onPressed: _submitForm,
-            ),
-            /*GestureDetector(
-              onTap: _submitForm,
-              child: Container(
-                color: Colors.green,
-                padding: EdgeInsets.all(5.0),
-                child: Text('My Button'),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode()); // close keyboard on tap
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
               ),
-            )*/
-          ],
+              RaisedButton(
+                child: Text('Save'),
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              ),
+              /*GestureDetector(
+                onTap: _submitForm,
+                child: Container(
+                  color: Colors.green,
+                  padding: EdgeInsets.all(5.0),
+                  child: Text('My Button'),
+                ),
+              )*/
+            ],
+          ),
         ),
       ),
     );
